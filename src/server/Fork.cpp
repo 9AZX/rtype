847cf97eabf810c1dcf0a136e13bf8f190416eda
Cédric Hennequin <cedric.hennequin@yahoo.fr>
2019-11-21 13:28:33 +0100

    Adding Fork class.

---
+++ src/server/Fork.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** Fork
+** File description:
+** Functions for the fork class.
+** @Author: Cédric Hennequin
+** @Date:   21-11-2019 12:58:43
+** @Last Modified by:   Cédric Hennequin
+** @Last Modified time: 21-11-2019 12:59:37
+*/
+
+#include "Fork.hpp"
+
+bool Fork::fork()
+{
+	return true;
+}
