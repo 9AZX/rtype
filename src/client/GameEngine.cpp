1cdc1dfb87eb1643755d27dd99e22ddec03b44a5
Ronan Gautiez <ronan.gautiez@epitech.eu>
2019-12-01 14:12:07 +0100

    Client: correction input

--- src/client/GameEngine.cpp
+++ src/client/GameEngine.cpp
@@ -18,26 +18,27 @@ GameEngine::~GameEngine()
 void GameEngine::TreatmentEvent(sf::Event event)
 {
     std::cout << "hol" << std::endl;
-    switch(event.key.code) {
-        case 22:
-            std::cout << "UP" << std::endl;
-            break;
-        case sf::Keyboard::S:
-            std::cout << "Down" << std::endl;
-            break;
-        case sf::Keyboard::A:
-            std::cout << "Left" << std::endl;
-            break;
-        case sf::Keyboard::D:
-            std::cout << "Right" << std::endl;
-            break;
-        case sf::Keyboard::Escape:
-            std::cout << "Pause" << std::endl;
-            break;
-        case sf::Keyboard::Space:
-            std::cout << "Space" << std::endl;
-            break;
-        default:
-           break;
+    switch (event.key.code)
+    {
+    case sf::Keyboard::W:
+        std::cout << "UP" << std::endl;
+        break;
+    case sf::Keyboard::S:
+        std::cout << "Down" << std::endl;
+        break;
+    case sf::Keyboard::A:
+        std::cout << "Left" << std::endl;
+        break;
+    case sf::Keyboard::D:
+        std::cout << "Right" << std::endl;
+        break;
+    case sf::Keyboard::Escape:
+        std::cout << "Pause" << std::endl;
+        break;
+    case sf::Keyboard::Space:
+        std::cout << "Space" << std::endl;
+        break;
+    default:
+        break;
     }
 }
