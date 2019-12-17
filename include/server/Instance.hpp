2e93093ee3f3c725af5d495c8ddddd1fdbc5b7e8
Jon Ihlas <jon.ihlas-mouesca@epitech.eu>
2019-12-17 13:12:43 +0100

    Fix compil on windows

--- include/server/Instance.hpp
+++ include/server/Instance.hpp
@@ -9,22 +9,23 @@
 ** @Last Modified time: 23-11-2019 15:50:17
 */
 
-#ifndef	_INSTANCE_HPP_
-#define	_INSTANCE_HPP_
+#ifndef _INSTANCE_HPP_
+#define _INSTANCE_HPP_
 
-#if	!defined(_WIN32) && !defined(_WIN64)
+#if !defined(_WIN32) && !defined(_WIN64)
 #include "Fork.hpp"
 #else
 #include <thread>
+#include <vector>
 #endif
 
-#define	SERVER_NEW_INSTANCE	"New server instance run (on pid: "
-#define	SERVER_PORT_NULL	0
+#define SERVER_NEW_INSTANCE "New server instance run (on pid: "
+#define SERVER_PORT_NULL 0
 
-#if	defined(_WIN32) || defined(_WIN64)
+#if defined(_WIN32) || defined(_WIN64)
 class Instance
 #else
-class Instance: public Fork
+class Instance : public Fork
 #endif
 {
 public:
@@ -36,16 +37,16 @@ public:
 	void setPort(unsigned short port) noexcept;
 	unsigned short getPort() const noexcept;
 	void run();
-#if	defined(_WIN32) || defined(_WIN64)
+#if defined(_WIN32) || defined(_WIN64)
 	unsigned int countInstances() const noexcept;
 #endif
 	void instance();
 
 private:
 	unsigned short _port = 0;
-#if	defined(_WIN32) || defined(_WIN64)
+#if defined(_WIN32) || defined(_WIN64)
 	std::vector<std::thread> _instances;
 #endif
 };
 
-#endif	/* !_INSTANCE_HPP_ */
+#endif /* !_INSTANCE_HPP_ */
--- src/server/Commands.cpp
+++ src/server/Commands.cpp
@@ -19,21 +19,22 @@ bool Commands::dispatcher(const int type, const std::vector<std::string> &args)
 {
 	bool value = true;
 
-	switch (type) {
-		case Commands::CMD_START:
-			value = this->start(args);
-			break;
-		case Commands::CMD_STATE:
-			value = this->state();
-			break;
-		case Commands::CMD_HELP:
-			value = this->help();
-			break;
-		case Commands::CMD_EXIT:
-			value = this->exit();
-			break;
-		default:
-			break;
+	switch (type)
+	{
+	case Commands::CMD_START:
+		value = this->start(args);
+		break;
+	case Commands::CMD_STATE:
+		value = this->state();
+		break;
+	case Commands::CMD_HELP:
+		value = this->help();
+		break;
+	case Commands::CMD_EXIT:
+		value = this->exit();
+		break;
+	default:
+		break;
 	}
 	return value;
 }
@@ -43,26 +44,32 @@ bool Commands::start(const std::vector<std::string> &args)
 	unsigned short port = 0;
 	CLI *cli = dynamic_cast<CLI *>(this);
 
-	if (args.size() != 1) {
+	if (args.size() != 1)
+	{
 		std::cerr << "Error: \"" << CMD_HELP_START << '\"' << std::endl;
 		return true;
 	}
 	port = static_cast<unsigned short>(std::atoi(args.front().c_str()));
 	cli->_instance->setPort(port);
 	cli->_instance->run();
+#if defined(_WIN32) || defined(_WIN64)
+	std::cout << SERVER_NEW_INSTANCE;
+#else
 	std::cout << SERVER_NEW_INSTANCE << cli->_instance->getLastPid();
+#endif
 	std::cout << ')' << std::endl;
 	cli->_instance->setPort(SERVER_PORT_NULL);
 	return true;
 }
 
-#if	!defined(_WIN32) && !defined(_WIN64)
+#if !defined(_WIN32) && !defined(_WIN64)
 bool Commands::state() noexcept
 {
 	CLI *cli = dynamic_cast<CLI *>(this);
 	std::vector<pid_t> process = cli->_instance->getProcess();
 
-	for (const auto &pid : process) {
+	for (const auto &pid : process)
+	{
 		std::cout << "Server instance: [" << pid << ']' << std::endl;
 	}
 	return true;
@@ -82,7 +89,8 @@ bool Commands::help() const noexcept
 {
 	std::cout << CMD_HELP__1 << std::endl;
 	std::cout << CMD_HELP__2 << std::endl;
-	std::cout << std::endl << CMD_HELP__3 << std::endl;
+	std::cout << std::endl
+			  << CMD_HELP__3 << std::endl;
 	std::cout << CMD_HELP__4 << std::endl;
 	std::cout << CMD_HELP__5 << std::endl;
 	std::cout << CMD_HELP__6 << std::endl;
