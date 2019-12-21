91c88d3ea3cd17e4cde5744162011d2f9d8914a4
Ronan Gautiez <ronan.gautiez@epitech.eu>
2019-12-21 15:55:46 +0100

    mob: add ia for mobs

--- include/entity/Mob.hpp
+++ include/entity/Mob.hpp
@@ -17,9 +17,21 @@ class Mob : public IEntity
 public:
     Mob();
     ~Mob();
+    void Ia();
+
+    void move(float x, float y) final;
+    void path(int path);
+    void shootingTime();
+
+    float getPosX() const final;
+    float getPosY() const final;
+    e_type getType() const final;
 
 protected:
     int _healthPoints;
+    int _movements = 0;
+    int _lastMovements = 0;
+    int _lastShoot = 0;
 
 private:
 };
