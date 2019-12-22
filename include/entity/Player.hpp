25d32dfaf57d869dfaf678f895c7fe87e7237e57
Cédric Hennequin <cedric.hennequin@yahoo.fr>
2019-12-22 18:50:01 +0100

    Adding getter and setter for the Player class.

--- include/entity/Player.hpp
+++ include/entity/Player.hpp
@@ -16,6 +16,7 @@
 #include "IEntity.hpp"
 
 #define	PLAYER_DEFAULT_PORT	7777
+#define	PLAYER_DEFAULT_LIVES	4
 
 using namespace Entity;
 
@@ -23,14 +24,27 @@ class Player : public IEntity
 {
 public:
 	Player();
-	~Player();
+	Player(const std::string &ip, const unsigned short port);
+	~Player() = default;
 
 public:
+	void setIpAddress(const std::string &address) noexcept;
+	const std::string &getIpAddress() const noexcept;
+	void setPort(const unsigned short port) noexcept;
+	const std::atomic<unsigned short> &getPort() const noexcept;
+	void setConnected(const bool connected) noexcept;
+	const std::atomic<bool> &isConnected() const noexcept;
+	void setId(const int id) noexcept;
+	const std::atomic<int> &getId() const noexcept;
+	void setLives(const int lives) noexcept;
+	const std::atomic<int> &getLives() const noexcept;
+
+private:
 	std::string _ip = "127.0.0.1";
-	unsigned short _port = PLAYER_DEFAULT_PORT;
+	std::atomic<unsigned short> _port;
 	std::atomic<bool> _connected;
 	std::atomic<int> _id;
-	std::atomic<int> _livesNb;
+	std::atomic<int> _lives;
 };
 
 #endif	/* !_PLAYER_HPP_ */
